7e17642cbed96d178aa071888e96262f34bba96f
Pedro Santos <pedro.san7os@gmail.com>
2017-02-01 12:04:32 +0000

    Ensure all unhandled exceptions are sent to bugsnag

--- config/bugsnag.rb
+++ config/bugsnag.rb
@@ -1,4 +1,5 @@
 require 'bugsnag'
+require 'English'
 
 BUGSNAG_API_KEY = ENV['BUGSNAG_API_KEY']
 
@@ -11,3 +12,7 @@ Bugsnag.configure do |c|
   c.hostname = "#{c.release_stage[0]}-#{ENV['DYNO'] || Socket.gethostname}-#{ENV['APP_NAME']}"
   c.app_version = ENV['APP_VERSION']
 end unless ['test', 'development'].include?(ENV['RACK_ENV'])
+
+at_exit do
+  $ERROR_INFO && Bugsnag.notify_or_ignore($ERROR_INFO)
+end
