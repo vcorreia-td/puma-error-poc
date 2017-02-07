b2c20b8e5161b6b8f8915a20400da59b108e5bd5
Tiago Sousa <tiago.joao@gmail.com>
2017-02-07 17:38:20 +0000

    Add initial Rubocop rules with exceptions for existing code

--- config/bugsnag.rb
+++ config/bugsnag.rb
@@ -5,7 +5,7 @@ BUGSNAG_API_KEY = ENV['BUGSNAG_API_KEY']
 
 Bugsnag.configure do |c|
   c.api_key = BUGSNAG_API_KEY
-  c.notify_release_stages = ['production', 'qa', 'staging']
+  c.notify_release_stages = %w(production qa staging)
   c.project_root = File.expand_path(File.dirname(__FILE__) + '/../')
   c.release_stage = ENV['RACK_ENV']
   c.logger = Logging.logger[Bugsnag]
@@ -18,7 +18,7 @@ Bugsnag.configure do |c|
     /session[_-]id/i,
     /csrf/i
   ]
-end unless ['test', 'development'].include?(ENV['RACK_ENV'])
+end unless %w(test development).include?(ENV['RACK_ENV'])
 
 at_exit do
   $ERROR_INFO && Bugsnag.notify_or_ignore($ERROR_INFO)
