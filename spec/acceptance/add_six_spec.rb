7501ebea94449730a4ee18280f108d8fcefb9868
Daniel Gomes <me@danielcsgomes.com>
2017-03-01 13:51:14 +0000

    Fix indentation in serializer Change the acceptance test expectation

--- spec/acceptance/add_six_spec.rb
+++ spec/acceptance/add_six_spec.rb
@@ -1,9 +1,8 @@
-require 'time'
 require 'acceptance_spec_helper'
 
 RSpec.describe 'Add six' do
   When { post(addsix_path, number: 33) }
   Then { expect(last_response.status).to be 200 }
   And  { expect(parsed_response['result']).to eq 39 }
-  And  { expect(Time.iso8601(parsed_response['at'])).to be_an_instance_of(Time) }
+  And  { expect(parsed_response['at']).to_not be nil }
 end
