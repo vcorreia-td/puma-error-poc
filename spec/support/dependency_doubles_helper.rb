5fcf2726116dd0407217de2f2646ec83d5ee317e
axfcampos <alexandre@talkdesk.com>
2017-03-14 18:32:50 +0000

    Correctly namespace spec support

--- spec/support/dependency_doubles_helper.rb
+++ spec/support/dependency_doubles_helper.rb
@@ -7,46 +7,49 @@
 
 # To avoid slowing down execution, we only require the dependencies as they are
 # needed.
+module MyServiceName
+  module Support
+    module DependencyDoublesHelper
+      DEPENDENCIES = {
 
-module DependencyDoublesHelper
-  DEPENDENCIES = {
+        # Example of a dependency declaration, :class & :instance
 
-    # Example of a dependency declaration, :class & :instance
+        # my_interactor: {
+        #   name:    'MyServiceName::Interactors::MyInteractor',
+        #   require: 'my_service_name/interactors/my_interactor',
+        #   type:    :instance,
+        # },
+        # my_factory: {
+        #   name:    'MyServiceName::MyFactory',
+        #   require: 'my_service_name/my_factory',
+        #   type:    :class,
+        # },
+      }.freeze
 
-    # my_interactor: {
-    #   name:    'MyServiceName::Interactors::MyInteractor',
-    #   require: 'my_service_name/interactors/my_interactor',
-    #   type:    :instance,
-    # },
-    # my_factory: {
-    #   name:    'MyServiceName::MyFactory',
-    #   require: 'my_service_name/my_factory',
-    #   type:    :class,
-    # },
-  }.freeze
+      def dependency_double(name, **args)
+        known_dependency = DEPENDENCIES[name]
 
-  def dependency_double(name, **args)
-    known_dependency = DEPENDENCIES[name]
+        if known_dependency
+          require known_dependency.fetch(:require)
+          name = known_dependency.fetch(:name)
 
-    if known_dependency
-      require known_dependency.fetch(:require)
-      name = known_dependency.fetch(:name)
+          case known_dependency.fetch(:type)
+          when :instance
+            instance_double(name, **args)
+          when :class
+            class_double(name, **args)
+          end
+        else
+          require 'colorized_string' # It's only ok to lazy require in specs. And stay in school, kids.
 
-      case known_dependency.fetch(:type)
-      when :instance
-        instance_double(name, **args)
-      when :class
-        class_double(name, **args)
+          puts ColorizedString["WARNING: Creating double for unknown dependency #{name.inspect}"].colorize(:red)
+          instance_double(name.to_s, **args)
+        end
       end
-    else
-      require 'colorized_string' # It's only ok to lazy require in specs. And stay in school, kids.
+    end
 
-      puts ColorizedString["WARNING: Creating double for unknown dependency #{name.inspect}"].colorize(:red)
-      instance_double(name.to_s, **args)
+    RSpec.configure do |c|
+      c.include DependencyDoublesHelper
     end
   end
-end
-
-RSpec.configure do |c|
-  c.include DependencyDoublesHelper
 end
