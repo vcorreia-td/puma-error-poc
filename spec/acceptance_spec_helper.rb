da831a468c2fed7e758a6c86c28b83fb47b31b18
axfcampos <alexandre@talkdesk.com>
2017-03-14 18:58:59 +0000

    Remove Helpers suffix
    Support prefix already supplies that meaning

--- spec/acceptance_spec_helper.rb
+++ spec/acceptance_spec_helper.rb
@@ -5,13 +5,13 @@ require 'rspec-hal'
 require 'rspec/given'
 require 'rspec'
 
-require 'support/api_helpers'
+require 'support/api'
 
 RSpec.configure do |conf|
   conf.include Rack::Test::Methods
   conf.include RSpec::Hal::Helpers
   conf.include RSpec::Hal::Matchers
-  conf.include MyServiceName::Support::APIHelpers
+  conf.include MyServiceName::Support::API
   conf.include GrapeRouteHelpers::NamedRouteMatcher
 end
 
