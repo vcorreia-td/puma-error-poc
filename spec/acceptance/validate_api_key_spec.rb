03c3a626499b858f677a1bda3d2fc9486dece45d
axfcampos <alexandre@talkdesk.com>
2017-05-21 10:45:32 +0100

    Update acceptance auth specs
    * Covers development exception
    * Multiple API_KEYS

--- spec/acceptance/validate_api_key_spec.rb
+++ spec/acceptance/validate_api_key_spec.rb
@@ -2,29 +2,41 @@ require 'acceptance_spec_helper'
 
 RSpec.describe 'api key validation' do
   context 'when no api key is configured' do
-    When { get root_path }
-    Then { expect(last_response.status).to be 200 }
+    When { expect { get root_path }.to raise_error }
+
+    context 'when in development mode' do
+      before do
+        ENV['RACK_ENV'] = 'development'
+      end
+
+      after do
+        ENV['RACK_ENV'] = 'test'
+      end
+
+      When { get root_path }
+      Then { expect(last_response.status).to be 200 }
+    end
   end
 
   context 'when an api key is configured' do
-    let(:api_key) { 'xyz' }
-
     before do
-      ENV['API_KEY'] = api_key
+      ENV['API_KEYS'] = 'abc,xyz'
     end
 
     after do
-      ENV['API_KEY'] = nil
+      ENV['API_KEYS'] = nil
     end
 
     context 'when the submitted key is valid' do
+      let(:submitted_key) { 'xyz' }
+
       context 'when the key is provided as a query param' do
-        When { get root_path, api_key: api_key }
+        When { get root_path, api_key: submitted_key }
         Then { expect(last_response.status).to be 200 }
       end
 
       context 'when the key is provided as a header' do
-        Given { header 'Api-Key', api_key }
+        Given { header 'X-Api-Key', submitted_key }
         When  { get root_path }
         Then  { expect(last_response.status).to be 200 }
       end
--- web/helpers/auth.rb
+++ web/helpers/auth.rb
@@ -1,14 +1,16 @@
+require_relative 'authenticator'
+
 module MyServiceName
   module Helpers
     module Auth
       extend Grape::API::Helpers
 
       def validate_api_key
-        key = headers['X-Api-Key'] || headers['Api-Key'] || params[:api_key]
-        match = ENV['API_KEY']
+        key = headers['X-Api-Key'] || params[:api_key]
+        match = ENV['API_KEYS']
 
-        return if match.nil?
-        return if key == match
+        return if ENV['RACK_ENV'] == 'development'
+        return if Authenticator.call(auth_keys: match, submitted_key: key)
 
         error!('Access denied due to invalid key', 401)
       end
